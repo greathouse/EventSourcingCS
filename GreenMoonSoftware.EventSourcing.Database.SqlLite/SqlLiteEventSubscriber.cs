aaa689b18cf36c4b4aa9f7bd1d2b0b17c3936f32
Robert Greathouse <greathouse.robert@gmail.com>
2019-05-15 15:30:41 -0500

    Cleanup

--- GreenMoonSoftware.EventSourcing.Database.SqlLite/SqlLiteEventSubscriber.cs
+++ GreenMoonSoftware.EventSourcing.Database.SqlLite/SqlLiteEventSubscriber.cs
@@ -1,7 +1,5 @@
 using System;
-using System.Data;
 using System.Data.SQLite;
-using System.Runtime.InteropServices;
 using GreenMoonSoftware.EventSourcing.Core.Event;
 using GreenMoonSoftware.EventSourcing.Database;
 
