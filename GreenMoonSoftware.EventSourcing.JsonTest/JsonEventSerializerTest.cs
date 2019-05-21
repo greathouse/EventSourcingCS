24626e68e9e8a417be62901fd44f45943a4ffd7e
Robert Greathouse <greathouse.robert@gmail.com>
2019-05-21 10:58:46 -0500

    Add Json Serializer test for lists

--- GreenMoonSoftware.EventSourcing.JsonTest/JsonEventSerializerTest.cs
+++ GreenMoonSoftware.EventSourcing.JsonTest/JsonEventSerializerTest.cs
@@ -1,7 +1,10 @@
 using System;
+using System.Collections.Generic;
+using System.Linq;
 using System.Text;
 using GreenMoonSoftware.EventSourcing.Core.Event;
 using GreenMoonSoftware.EventSourcing.Json;
+using Microsoft.VisualStudio.TestPlatform.ObjectModel.Engine.ClientProtocol;
 using Newtonsoft.Json;
 using Xunit;
 
@@ -12,7 +15,40 @@ namespace GreenMoonSoftware.EventSourcing.JsonTest
         [Fact]
         public void ShouldBeAbleToSerializerAndDeserialize()
         {
-            var obj = new TestSimpleObject(Guid.NewGuid().ToString())
+            var obj = ValidSimpleObject();
+            
+            var serializer = new JsonEventSerializer();
+            var serial = serializer.Serialize(obj);
+            var actual = (TestSimpleObject)serializer.Deserialize(obj.Type, serial);
+            
+            Assert.Equal(obj.StringType, actual.StringType);
+            Assert.Equal(obj.TrueBoolean, actual.TrueBoolean);
+            Assert.Equal(obj.FalseBoolean, actual.FalseBoolean);
+            Assert.Equal(obj.Double, actual.Double);
+            Assert.Equal(obj.Integer, actual.Integer);
+        }
+
+        [Fact]
+        public void ShouldSerializeLists()
+        {
+            var obj = new TestEventWithList(Guid.NewGuid().ToString())
+            {
+                ListOfObjects = new []
+                {
+                    ValidSimpleObject(), ValidSimpleObject()
+                }
+            };
+            
+            var serializer = new JsonEventSerializer();
+            var serial = serializer.Serialize(obj);
+            var actual = (TestEventWithList)serializer.Deserialize(obj.Type, serial);
+            
+            Assert.Equal(obj.ListOfObjects.Count(), actual.ListOfObjects.Count());
+        }
+
+        private static TestSimpleObject ValidSimpleObject()
+        {
+            return new TestSimpleObject(Guid.NewGuid().ToString())
             {
                 StringType = "TestString",
                 TrueBoolean = true,
@@ -20,16 +56,6 @@ namespace GreenMoonSoftware.EventSourcing.JsonTest
                 Double = 69.69d,
                 Integer = 69
             };
-            var serializer = new JsonEventSerializer();
-
-            var serial = serializer.Serialize(obj);
-            var actual = (TestSimpleObject)serializer.Deserialize(typeof(TestSimpleObject).FullName, serial);
-            
-            Assert.Equal(obj.StringType, actual.StringType);
-            Assert.Equal(obj.TrueBoolean, actual.TrueBoolean);
-            Assert.Equal(obj.FalseBoolean, actual.FalseBoolean);
-            Assert.Equal(obj.Double, actual.Double);
-            Assert.Equal(obj.Integer, actual.Integer);
         }
     }
     
@@ -42,6 +68,15 @@ namespace GreenMoonSoftware.EventSourcing.JsonTest
         public int Integer { get; set; }
 
         public TestSimpleObject(string aggregateId) : base(aggregateId, typeof(TestSimpleObject).FullName)
+        {
+        }
+    }
+
+    public class TestEventWithList : AbstractEvent
+    {
+        public IEnumerable<TestSimpleObject> ListOfObjects { get; set; }
+        
+        public TestEventWithList(string aggregateId) : base(aggregateId, typeof(TestEventWithList).FullName)
         {
         }
     }
